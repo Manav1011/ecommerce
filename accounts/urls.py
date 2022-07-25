7c68bd620e2a473cb2f64f1eefc4256a3bde857a
manavshah <manavshah1011.ms@gmail.com>
2022-07-25 11:58:20 +0530

    till live

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name='accounts'
 
 urlpatterns=[
-    re_path(r'^$',views.SignUpView,name='signup'),
-    path('active/<str:username>/<str:token>',views.active_account,name='active'),
-    re_path(r'^Activated/$',views.activated,name='activated'),
+    re_path(r'^$',views.SignupProcess.SignUpView,name='signup'),
+    path('active/<str:username>/<str:token>',views.SignupProcess.active_account,name='active'),
+    re_path(r'^Activated/$',views.SignupProcess.activated,name='activated'),
     re_path(r'^username_for_reset_password/$',views.username_for_reset_password,name='username_for_reset_password'),
     path('reset_password_page/<str:username>/<str:token>',views.reset_password_page,name='reset_password_page'),
     re_path(r'^reset_password/$',views.reset_password,name='reset_password'),
